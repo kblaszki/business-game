f453b60bc71763eef08aeb50f6dcc8096d129f20
KrystianDev <blaszkiewicz121@protonmail.com>
2022-06-07 23:16:11 +0200

    Add sfml to project.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 //
 // Created by Dev on 07.06.2022.
 //
-
-#include "LibraryCode.h"
-
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    int a, b;
-    std::cin >> a >> b;
-    std::cout << sum(a, b);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
     return EXIT_SUCCESS;
 }
