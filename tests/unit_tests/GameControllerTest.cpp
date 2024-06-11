b07f4e48da570cf466f6fdb22feb4266641ddf78
kblaszki <blaszkiewicz121@protonmail.com>
2024-06-11 21:15:30 +0200

    Add GameController unit test.

---
+++ tests/unit_tests/GameControllerTest.cpp
@@ -0,0 +1,15 @@
+/* Created by kblaszki */
+
+#include <controllers/GameController.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace ::testing;
+struct GameControllerShould : Test
+{
+};
+
+TEST_F(GameControllerShould, example)
+{
+    EXPECT_TRUE(true);
+}
