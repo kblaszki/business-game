b77f82da100ea017e31765dd8dbdbf280279af9f
kblaszki <blaszkiewicz121@protonmail.com>
2024-06-29 23:23:08 +0200

    Update EventManagers interface.

--- src/managers/EventManagerI.hpp
+++ src/managers/EventManagerI.hpp
@@ -3,14 +3,64 @@
 
 #include <SFML/Window/Event.hpp>
 
+enum class EventManagerOf
+{
+    None,
+    GameExit,
+    GameWindow,
+    Keyboard,
+    Mouse,
+    Joystick,
+    Touch,
+    Sensor
+};
+
+EventManagerOf getEventManagerOf(sf::Event::EventType event)
+{
+    switch(event)
+    {
+        case sf::Event::Closed:
+            return EventManagerOf::GameExit;
+        case sf::Event::LostFocus:
+        case sf::Event::GainedFocus:
+        case sf::Event::Resized:
+            return EventManagerOf::GameWindow;
+        case sf::Event::KeyPressed:
+        case sf::Event::KeyReleased:
+        case sf::Event::TextEntered:
+            return EventManagerOf::Keyboard;
+        case sf::Event::MouseWheelScrolled:
+        case sf::Event::MouseButtonPressed:
+        case sf::Event::MouseButtonReleased:
+        case sf::Event::MouseMoved:
+        case sf::Event::MouseEntered:
+        case sf::Event::MouseLeft:
+            return EventManagerOf::Mouse;
+        case sf::Event::JoystickButtonPressed:
+        case sf::Event::JoystickButtonReleased:
+        case sf::Event::JoystickMoved:
+        case sf::Event::JoystickConnected:
+        case sf::Event::JoystickDisconnected:
+            return EventManagerOf::Joystick;
+        case sf::Event::TouchBegan:
+        case sf::Event::TouchMoved:
+        case sf::Event::TouchEnded:
+            return EventManagerOf::Touch;
+        case sf::Event::SensorChanged:
+            return EventManagerOf::Sensor;
+        default:
+            return EventManagerOf::None;
+    }
+}
+
 struct EventManagerI
 {
     virtual ~EventManagerI() = default;
     virtual void handleEvent(const sf::Event& event) = 0;
 };
 
-template<sf::Event::EventType EventType>
+template<EventManagerOf TYPE>
 struct EventManager : public EventManagerI
 {
-    static constexpr sf::Event::EventType EVENT_TYPE = EventType;
+    static constexpr EventManagerOf EVENT_MANAGER_TYPE = TYPE;
 };
--- src/managers/EventManagers.hpp
+++ src/managers/EventManagers.hpp
@@ -8,27 +8,27 @@
 
 template<typename T>
 concept HasEventType = requires {
-    { T::EVENT_TYPE } -> std::convertible_to<sf::Event::EventType>;
+    { T::EVENT_MANAGER_TYPE } -> std::convertible_to<EventManagerOf>;
 };
 template<typename T>
-concept IsBaseOfEventManager = HasEventType<T> && std::is_base_of_v<EventManager<T::EVENT_TYPE>, T>;
+concept IsBaseOfEventManager = HasEventType<T> && std::is_base_of_v<EventManager<T::EVENT_MANAGER_TYPE>, T>;
 
 class EventManagers
 {
 public:
     virtual ~EventManagers() = default;
 
-    template<sf::Event::EventType EVENT_TYPE>
-    EventManager<EVENT_TYPE>& get()
+    template<EventManagerOf EVENT_MANAGER_TYPE>
+    EventManager<EVENT_MANAGER_TYPE>& get()
     {
 
-        return static_cast<EventManager<EVENT_TYPE>&>(get(EVENT_TYPE));
+        return static_cast<EventManager<EVENT_MANAGER_TYPE>&>(get(EVENT_MANAGER_TYPE));
     }
 
 protected:
-    EventManagerI& get(sf::Event::EventType eventType)
+    EventManagerI& get(EventManagerOf type)
     {
-        auto manager = eventManagers.find(eventType);
+        auto manager = eventManagers.find(type);
         if(eventManagers.end() == manager or nullptr == manager->second)
         {
             throw std::runtime_error("No event manager for this event type");
@@ -36,7 +36,7 @@ protected:
         return *manager->second;
     }
 
-    std::unordered_map<sf::Event::EventType, std::unique_ptr<EventManagerI>> eventManagers;
+    std::unordered_map<EventManagerOf, std::unique_ptr<EventManagerI>> eventManagers;
 };
 
 class LordOfEventManagers : public EventManagers
@@ -48,23 +48,23 @@ public:
     {
 
         auto eventManager = std::make_unique<EVENT_MANAGER>(std::forward<Args>(args)...);
-        auto manager = eventManagers.find(EVENT_MANAGER::EVENT_TYPE);
+        auto manager = eventManagers.find(EVENT_MANAGER::EVENT_MANAGER_TYPE);
         if(eventManagers.end() not_eq manager)
         {
             throw std::runtime_error("Event manager already exists");
         }
-        eventManagers.emplace(EVENT_MANAGER::EVENT_TYPE, std::move(eventManager));
+        eventManagers.emplace(EVENT_MANAGER::EVENT_MANAGER_TYPE, std::move(eventManager));
     }
 
     template<typename EVENT_MANAGER>
     void emplace(std::unique_ptr<EVENT_MANAGER>&& eventManager)
         requires IsBaseOfEventManager<EVENT_MANAGER>
     {
-        auto manager = eventManagers.find(EVENT_MANAGER::EVENT_TYPE);
+        auto manager = eventManagers.find(EVENT_MANAGER::EVENT_MANAGER_TYPE);
         if(eventManagers.end() not_eq manager)
         {
             throw std::runtime_error("Event manager already exists");
         }
-        eventManagers.emplace(EVENT_MANAGER::EVENT_TYPE, std::move(eventManager));
+        eventManagers.emplace(EVENT_MANAGER::EVENT_MANAGER_TYPE, std::move(eventManager));
     }
 };
